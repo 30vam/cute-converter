a2392c71f9353a16d266515dcb65580d7e064f69
Sina <97638468+30vam@users.noreply.github.com>
2023-10-04 21:14:29 +0330

    UI prototype

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSize>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //WIDGET MEMBERS
+
+    //Set the starting app size as default. You can use Design tab to change the window size manually.
+    this->setFixedSize(this->geometry().width(), this->geometry().height());
+
 }
 
 MainWindow::~MainWindow()
