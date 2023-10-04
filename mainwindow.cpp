ba1b1152404958e7380903acbbcd0c07e4a3df8f
Sina <97638468+30vam@users.noreply.github.com>
2023-10-04 21:42:49 +0330

    UI prototype

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QSize>
+#include <QGroupBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,6 +13,11 @@ MainWindow::MainWindow(QWidget *parent)
 
     //Set the starting app size as default. You can use Design tab to change the window size manually.
     this->setFixedSize(this->geometry().width(), this->geometry().height());
+
+    //scrollarea test
+    QGroupBox *testGroupBox = new QGroupBox("Test box", this);
+    testGroupBox->setFixedHeight(2000);
+    ui->conversionScrollArea->setWidget(testGroupBox);
 
 }
 
