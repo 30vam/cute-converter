49fcd1a6f3dcfc81a41d4327c2162ae6945d3344
Sina Kh <97638468+30vam@users.noreply.github.com>
2023-10-23 01:19:34 +0330

    new windows

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
+            //
         }
     }
     MainWindow w;
