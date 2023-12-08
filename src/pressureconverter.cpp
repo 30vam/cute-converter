f47adc3d1e82f06ba776a37051ea8f736a9bb26b
Sina Kh <97638468+30vam@users.noreply.github.com>
2023-12-09 00:16:08 +0100

    completed pressure converter

--- src/pressureconverter.cpp
+++ src/pressureconverter.cpp
@@ -1,8 +1,12 @@
 #include "pressureconverter.h"
 
 PressureConverter::PressureConverter(QWidget *parent)
-    : AbstractConverter({ {"Pa(Pascal)", 1}, {"m/min(meter per minute)", 60}, {"km/s", 0.001}, {"km/min", 0.06}, {"km/h(kilometer per hour)", 3.6},
-                         {"ft/s(foot per second)", 3.281}, {"ft/min(foot per minute)", 196.85}, {"mi/h(mile per hour)", 2.237}, {"mach", 0.00291545}, {"knot", 1.944} },
+    : AbstractConverter({ {"Pa(Pascal)", 1}, {"hPa(Hectopascal)", 0.01}, {"kPa(Kilopascal)", 0.001}, {"MPa(MegaPascal)", 1e-6}, {"atm(standard atmosphere)", 9.86923e-6},
+                        {"Bar", 1e-5}, {"mBar(milliBar)", 0.01}, {"psi(pound per square inch)", 0.000145038}, {"psf(pound per square foot)", 0.020885},
+                        {"ksi(kilopound force/inch²)", 1.45038e-7}, {"kgf/cm²(kilogram force/centimeter²)", 1.01972e-5}, {"kgf/m²(kilogram force/meter²)", 0.101972},
+                        {"mmHg(millimeter of mercury)", 0.00750062}, {"cmHg(centimeter of mercury)", 0.000750062}, {"inchHg(inch of mercury)", 0.0002953},
+                        {"mmH₂O(millimeter of water)", 0.10197}, {"cmH₂O(centimeter of water)", 0.0101972}, {"inchH₂O(inch of water)", 0.00401865},
+                        {"Torr", 0.00750062}, {"mTorr(millTorr)", 7.50062 } },
                         "Pressure", parent)
 {
 
