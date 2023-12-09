1d4493ce392079fc1ae0320d7afa37761cb142c9
Sina Kh <97638468+30vam@users.noreply.github.com>
2023-12-09 18:28:42 +0100

    Completed about window

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,11 +1,15 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
+#include <QLayout>
 
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+
+    this->setFixedSize(this->size());  //Locked dialog size
+    this->setWindowTitle("About");  //Dialog name
 }
 
 AboutDialog::~AboutDialog()
